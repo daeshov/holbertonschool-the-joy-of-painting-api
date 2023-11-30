d7500d4524f4eb0921c5c9520adce24dcbf34c6f
darah thomas <Jdarahthomas@gmail.com>
2023-11-30 16:45:17 -0600

    still working

--- index.js
+++ index.js
@@ -1,61 +1,31 @@
-const fs = require('fs').promises;
-const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 const csv = require('csv-parser');
-const path = require('path');
 
-// Connect to the SQLite database
-const db = new sqlite3.Database('./sql_server/joy_of_painting_series.db');
 
-async function insertDataFromCSV(table, csvFileName) {
-  const csvFilePath = path.join(__dirname, 'datasets', `${csvFileName}.csv`);
+const processCSV = (filePath, hasHeaders) => {
+  const data = [];
 
-  try {
-    // Read CSV data
-    const data = await fs.readFile(csvFilePath, 'utf8');
-    const rows = data.trim().split('\n').map(row => row.split(','));
+  fs.createReadStream(filePath)
+    .pipe(csv({ headers: hasHeaders }))
+    .on('data', (row) => {
+      // Process each row and extract into multiple columns
+      const title = String(row[0]).replace(/\([^)]*\)/, '').trim();
+      const date = /\((.*?)\)/.exec(row[0]);
+      const formattedDate = date ? date[1] : '';
+      const paint_index = row.painting_index.trim();
 
-    // Insert data into the database
-    const columns = rows[0].map(col => `"${col.replace(/"/g, '')}"`).join(', ');
-    const placeholders = rows[0].map(() => '?').join(', ');
-    const sql = `INSERT INTO ${table} (${columns}) VALUES (${placeholders})`;
-
-    console.log('Generated SQL query:', sql);
-
-    db.serialize(() => {
-      const stmt = db.prepare(sql);
-
-      rows.slice(1).forEach(row => {
-        stmt.run(row);
+      data.push({
+        title: title,
+        date: formattedDate.trim(),
+        paint_index: paint_index,
       });
-
-      stmt.finalize();
+    })
+    .on('end', () => {
+      // After reading all data
+      console.log(data);
+      // Perform further processing or insert into the database here
     });
+};
 
-    console.log(`Data inserted into ${table} from ${csvFilePath}`);
-  } catch (error) {
-    console.error(`Error reading or inserting data for ${table}: ${error.message}`);
-  }
-}
-
-async function main() {
-  // Insert data into episodes table
-  await insertDataFromCSV('episodes', 'The Joy Of Painting - Episode Dates');
-
-  // Insert data into colors table
-  await insertDataFromCSV('colors', 'The Joy Of Painting - Colors Used');
-
-  // Insert data into subject_matters table
-  await insertDataFromCSV('subject_matters', 'The Joy Of Painting - Subject Matter');
-
-  // Close the database connection after all operations have finished
-  db.close((err) => {
-    if (err) {
-      console.error(`Error closing the database connection: ${err.message}`);
-    } else {
-      console.log('All operations completed. Database connection closed.');
-    }
-  });
-}
-
-// Call the main function
-main();
+processCSV('datasets/The Joy Of Painting - Episode Dates.csv', false);
+processCSV('datasets/The Joy Of Painting - Colors Used.csv', true)
